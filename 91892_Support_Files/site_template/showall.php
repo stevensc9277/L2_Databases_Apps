c07a5241afea920f6b6a0011f3724e8b54039281
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-08-13 11:43:28 +1200

    added show all and changed hover color

---
+++ 91892_Support_Files/site_template/showall.php
@@ -0,0 +1,14 @@
+<?php include("top_bit.php") ?>            
+        <div class="box main">
+            <h2>All Results</h2>
+            
+            
+            <p>
+                Results will go here
+            </p>
+            
+
+            
+        </div> <!-- / main -->
+
+<?php include("bottom_bit.php") ?>
