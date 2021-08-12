00313b5853a5f6dbfa5d39f000cd05ec025e8f05
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-08-12 13:25:34 +1200

    put right link to site

--- 91892_Support_Files/site_template/index.php
+++ 91892_Support_Files/site_template/index.php
@@ -4,7 +4,7 @@
             
             
             <p>
-                This database includes some of the most popular apps available for mobile devices. The <a href="https://www.apple.com/app-store/">original dataset</a> can be found at the preceding link.
+                This database includes some of the most popular apps available for mobile devices. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">original dataset</a> can be found at the preceding link.
             </p>
             
 
