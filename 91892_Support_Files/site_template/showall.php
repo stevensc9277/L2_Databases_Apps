27d6ae163b981ae271474ef6027cc49dcad11b8a
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-08-13 11:55:06 +1200

    show all displays stuff now

--- 91892_Support_Files/site_template/showall.php
+++ 91892_Support_Files/site_template/showall.php
@@ -1,11 +1,48 @@
-<?php include("top_bit.php") ?>            
+<?php include("top_bit.php");
+
+    $find_sql = "SELECT * FROM `game_details`";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>            
+
         <div class="box main">
             <h2>All Results</h2>
             
             
-            <p>
-                Results will go here
-            </p>
+            <?php
+            
+            if($count < 1){
+                
+                ?>
+            
+            <div class = "error">
+            
+            Sorry! There are no results that match your search. Please use the search box in the side bar to try again.
+                
+            </div> <!-- end error -->
+            
+            <?php
+            }
+            
+            else{
+                do
+                {
+                   
+                    ?>
+            <!-- Results go here -->
+            <div class="results">
+                 You have results!
+            </div>
+            
+            <?php
+                } // end results 'do'
+                
+                while($find_rs=mysqli_fetch_assoc($find_query));
+            } // end else
+                
+            ?>
             
 
             
