e46e4b314567c844bb318b2e0d457f24af3c7c63
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-08-13 12:30:09 +1200

    just try to add star code

---
+++ 91892_Support_Files/site_template/showall_v2.php
@@ -0,0 +1,75 @@
+<?php include("top_bit.php");
+
+    $find_sql = "SELECT * FROM `game_details`
+    JOIN genre ON (game_details.GenreID = genre.GenreID)
+    JOIN developer ON (game_details.DeveloperID = developer.developerID)
+    
+    ";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>            
+
+        <div class="box main">
+            <h2>All Results</h2>
+            
+            
+            <?php
+            
+            if($count < 1){
+                
+                ?>
+            
+            <div class = "error">
+            
+            Sorry! There are no results that match your search. Please use the search box in the side bar to try again.
+                
+            </div> <!-- end error -->
+            
+            
+            
+            <?php
+            }
+            
+            else{
+                do
+                {
+                   
+                    ?>
+            <!-- Results go here -->
+            <div class="results">
+                <span class="sub_heading">
+                <a href="<?php echo $find_rs['URL']; ?>"><?php echo $find_rs['Name']; ?></a>
+                </span>   
+                <p>
+                    <b>Genre</b>:
+                    <?php echo $find_rs['Genre'] ?>
+                
+                <br />
+                <b>Developer</b>:
+                    <?php echo $find_rs['DevName'] ?>
+                    
+                <br />
+                <b>Rating</b>:
+                    <?php echo $find_rs['User Rating']; ?> (based on <?php echo $find_rs['Rating Count'];?> votes)
+                </p>
+                
+                <hr />
+                <?php echo $find_rs['Description']; ?>
+            </div> <!-- results -->
+            
+            <br />
+            <?php
+                } // end results 'do'
+                
+                while($find_rs=mysqli_fetch_assoc($find_query));
+            } // end else
+                
+            ?>
+            
+
+            
+        </div> <!-- / main -->
+
+<?php include("bottom_bit.php") ?>
