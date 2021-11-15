2f91a2009c96f518755ddb089d23a1ee358f0193
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-11-16 10:30:22 +1300

    finish at home

--- 91892_Support_Files/site_template/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('display_errors', 'On'); //change to 'Off' when live
-
-define ("DB_NAME", "stevensc9277_L2_Appstore_Games");
-define ("DB_USERNAME", "_stevensc9277");
-define ("DB_PASSWORD", "y3F99iZfLXIRBPDo");
-define ("DB_HOST", "localhost");
-?>
